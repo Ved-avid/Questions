66acfde6319b32ac9edee65993a4a74fc6d46bb4
|-- Master Shifu --| <64973965+Ved-avid@users.noreply.github.com>
2021-08-19 19:29:23 +0530

    Add files via upload

---
+++ CircleOfFriend.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import static java.lang.System.out;
+
+public class CircleOfFriend {
+    static public void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n =  sc.nextInt();
+        while(n-->0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int range = (int)Math.abs(a-b)*2;
+            if(a>range || b> range || c> range){
+                out.println(-1);
+            }else{
+                int diff = (int)Math.abs(a-b);
+                out.println(c<=diff? c+diff:c-diff);
+            }
+        }
+
+    
+           
+    }    
+}
